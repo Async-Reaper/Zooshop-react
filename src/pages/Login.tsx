6190e3faf698904ae05cbdeef111d7a29733149e
ASRync <m_igor_r@mail.ru>
2022-06-01 15:31:55 +0700

    create styles for login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,18 @@
+import styled from '@emotion/styled'
 import React, { FC } from 'react'
 import LoginForm from '../components/Forms/LoginForm'
 
+const LoginWrapper = styled('div')({
+  display: 'flex',
+  justifyContent: 'center',
+  marginTop: 35
+})
+
 const Login: FC = () => {
   return (
-    <div>
+    <LoginWrapper>
       <LoginForm />
-    </div>
+    </LoginWrapper>
   )
 }
 
