f55b9a477978ac9265ed9d2e296c9c40c0fe10aa
ASRync <m_igor_r@mail.ru>
2022-06-01 17:01:53 +0700

    create hook for validation

---
+++ src/hooks/useValidation.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+
+export const useValidation = (value: string, validations: any) => {
+    const [isEmpty, setIsEmpty] = useState(true);
+    const [minLength, setMinLength] = useState(true);
+    
+
+    useEffect(() => {
+        for (const validation in validations) {
+            switch(validation) {
+                case 'minLength' :
+                    value.length > validations[validation] ? setMinLength(false) : setMinLength(true)
+                break;
+                case 'isEmpty' :
+                    value ? setIsEmpty(false) : setIsEmpty(true)
+            }
+        }
+    }, [isEmpty, minLength])
+
+    return {
+        isEmpty,
+        minLength
+    }
+}
