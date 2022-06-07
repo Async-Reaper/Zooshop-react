1797fa8d5546ef5d6a401d82fbedc1c28f078a70
ASRync <m_igor_r@mail.ru>
2022-06-07 16:30:16 +0700

    create table cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,17 +1,20 @@
+import { Button } from '@mui/material'
 import React, { FC } from 'react'
 import { useTypedDispatch } from '../../hooks/useTypedDispatch'
 import { useTypedSelector } from '../../hooks/useTypedSelector'
 import { setStateCartModal } from '../../store/reducers/modalWindowsSlice'
 import ModalWindow from '../UI/ModalWindow/ModalWindow'
-
+import CartTable from './CartTable/CartTable'
 const Cart: FC = () => {
     const { cartModal } = useTypedSelector(state => state.modalWindows)
     const dispatch = useTypedDispatch()
 
+    
     return (
         <ModalWindow openModal={cartModal} closeModal={() => dispatch(setStateCartModal(false))} > 
             <div>
-                entry content
+                <CartTable />
+                <Button variant='outlined'>Оформить заказ</Button>
             </div>
         </ModalWindow>
     )
