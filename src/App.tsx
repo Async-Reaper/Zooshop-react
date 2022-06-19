e171594ef35abd025a69915b5940044ff558d689
ASRync <m_igor_r@mail.ru>
2022-06-19 19:33:40 +0700

    fix localstorage cart

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
 import AppRouter from './components/AppRouter';
 import HeaderApp from './components/UI/Header/HeaderApp';
+import { useTypedSelector } from './hooks/useTypedSelector';
 
 function App() {
+  const {cart} = useTypedSelector(state => state.cart)
+
+  localStorage.setItem('cart', JSON.stringify(cart))
+  
   return (
     <BrowserRouter>
       <div className="App">
